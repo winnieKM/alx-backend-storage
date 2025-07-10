c05af224d569150b214464db090670a20c47c432
Winniekathomi <winniemorris254@gmail.com>
2025-07-10 21:42:32 +0300

    Task 0: Store data in Redis with randomly generated key

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """
-Test file for Cache class get method.
+Main file to test Cache class
 """
 
+import redis
 from exercise import Cache
 
 cache = Cache()
 
-# Store a string
-key_str = cache.store("Hello Redis")
-print("Raw:", cache.get(key_str))               # Should print b'Hello Redis'
-print("As str:", cache.get(key_str, str))       # Should print 'Hello Redis'
+data = b"hello"
+key = cache.store(data)
+print("Stored key:", key)
 
-# Store an integer
-key_int = cache.store(123)
-print("Raw:", cache.get(key_int))               # Should print b'123'
-print("As int:", cache.get(key_int, int))       # Should print 123
+# Check if the data is stored in Redis
+local_redis = redis.Redis()
+print("Stored value:", local_redis.get(key))
